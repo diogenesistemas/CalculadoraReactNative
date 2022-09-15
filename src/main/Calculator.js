5bf4a507b6f6ba9b5a75865e6d24970f60c00500
Diogenes Bezerra Pereira <dibpereira@rd.com.br>
2022-09-15 11:40:12 -0300

    fix: erro no display

--- src/main/Calculator.js
+++ src/main/Calculator.js
@@ -11,125 +11,55 @@ function Calculator(props) {
     const [number, setNumber] = useState("")
     const [expression, setExpression] = useState()
     const [display, setDisplay] = useState("")
-    const [operation, setOperation] = useState("")
     const [result, setResult] = useState(0)
 
     const addDigit = (digit) => {
         let n = number + digit;
-        // setExpression(newExpression)
-        // console.log(newExpression)
         setNumber(n);
         let newExpression = expression + n
-
-        // let newExpression = expression + operation
         setExpression(newExpression)
-        // console.log(n)
-        // console.warn(newExpression)
-        setDisplay(newExpression);
-
-        // let newExpression = expression
-        // let newExpression = executeOperation(...expression) + n
-        // let newExpression = expression + n
-
-       
-        // setExpression(expression)
-        
+        setDisplay(newExpression);        
         calcOperation(newExpression)
-
-       
-        // console.warn(newExpression)
-
-
-        // console.warn(eval(resultado))
-    }
-
-    const calcOperation = (e) =>{
-        let calc = expression != "" ? eval(e) : "";
-        setResult(calc)
     }
 
     const addOperation = (operation) => {
-        let newOperation = ""
-       
 
         let newExpression = expression
         // let ns = palavra.replace(0, palavra.length-1,"")
-        // console.log(ns)
+        console.warn(newExpression)
 
         if (lastDigitIsNotANumber(newExpression)){
-            newOperation = operation
+            newExpression = newExpression.substr(0, newExpression.length-1)
         }
-
-        setOperation(newOperation)
-        // let newExpression = expression + operation
-        newExpression = lastDigitIsNotANumber(newExpression) ? newExpression.substr(0, newExpression.length-1) + operation : expression + operation
+        newExpression += operation
         setExpression(newExpression)
-        setDisplay(newExpression)
-        // let newDisplay = display + operation
-        // setDisplay(newDisplay)
-        // setOperation(operation);
-        // let newExpression = expression  + number
-        // console.log(newExpression)
-        // setExpression(newExpression)
-        // setDisplay(newExpression)
-        // console.log(operation)
-        // newExpression.push(number)
-        // newExpression.push(operation)
-
-        // let newDisplay = display+operation
-        // console.warn(newDisplay)
-     
-
-
-        
-        // if (lastDigitIsNotANumber(executeOperation(...newExpression))){
-            
-        // }
-
-
-        // console.warn(executeOperation(...newExpression))
-
-
-        // setExpression(newExpression)
         setNumber("")
+        setDisplay(newExpression)
 
 
     }
-
-    function executeOperation ()  {
-
-        // for (let index = 0; index < expression.length; index++) {
-        //     const element = array[index];
-        //     console.warn(element)
-
-        // }
-
-        return [...arguments].reduce((sum, nr) => sum + nr, "");
+    
+    const calcOperation = (e) =>{
+        let calc = expression != "" ? eval(e) : 0;
+        setResult(calc)
     }
 
-
-
-    // if (lastDigitIsNumber(expression)){
-    //     console.warn(expression);
-    //     // calcResult(expression);
-    //     // console.warn("Não é")
-    // }
 
     const reset = () => {
         setNumber("")
         setExpression("")
-        setOperation("")
+        // setOperation("")
         setDisplay("");
-        setResult("");
+        setResult(0);
     }
 
-    const calcResult = ()=>{
+    const calcResult = (props)=>{
         reset()
         setExpression(result)
         setDisplay(result)  
     }
 
-    function lastDigitIsNotANumber(value){
+    const lastDigitIsNotANumber = (value) => {
        return isNaN(value.substr(value.length-1));
     }
 
